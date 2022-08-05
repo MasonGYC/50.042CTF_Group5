a9fbe8dbab22dcf800792447bb7c29e026f39118
zach <zach_lim@mymail.sutd.edu.sg>
2022-08-05 12:42:47 +0800

    Combined into 1 file for submission

--- Group5CTF.py
+++ Group5CTF.py
@@ -1,8 +1,5 @@
 import base64
 
-flag = "fcs22{wahhhhhhhh_you_found_the_flag!!!whooohooo}"
-key = "counterstrike"
-
 class VariantVigenere:
     def __init__(self):
         self._chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
@@ -30,11 +27,6 @@ class VariantVigenere:
 
     @staticmethod
     def string_to_base64(flag):
-        """
-        Covert the flag to a base64 text
-        :param str flag: plaintext flag
-        :return: str flag_base64: flag encoded by base64
-        """
         flag_byte = flag.encode("utf-8")
         flag_byte_base64 = base64.b64encode(flag_byte)
         flag_base64 = flag_byte_base64.decode("utf-8")
@@ -42,29 +34,14 @@ class VariantVigenere:
 
     @staticmethod
     def base64_to_string(flag_base64):
-        """
-        Covert the base64 text to utf-8 flag string
-        :param str flag_base64:flag encoded by base64
-        :return: str flag: plaintext flag
-        """
         flag = base64.b64decode(flag_base64).decode("utf-8")
         return flag
 
     def get_shift_amount(self, char):
-        """
-        Get the shift amount with the give character key
-        :param str key: a single letter, digit or +, -, =
-        :return: int shift_amount: the shift amount with the given key
-        """
         shift_amount = self._chars.index(char)
         return shift_amount
 
     def get_shift_list(self, key):
-        """
-        Get the shift list with the give key string
-        :param str key: a string
-        :return: list shift_list: the shift list with the give key string
-        """
         shift_list = []
         for k in key:
             shift_amount = self.get_shift_amount(k)
@@ -72,80 +49,139 @@ class VariantVigenere:
         return shift_list
 
     def shift_char_encrypt(self, char, shift_amount):
-        """
-        Shift the character for encryption
-        :param str char: character to be shifted
-        :param int shift_amount: the amount of shift in vigenere cipher
-        :return: str shifted_char: char after shifting
-        """
         original_position = self._chars.index(char)
         shift_position = (original_position + shift_amount) % len(self._chars)
         shifted_char = self._chars[shift_position]
         return shifted_char
 
     def shift_char_decrypt(self, char, shift_amount):
-        """
-        Shift the character for decryption
-        :param str char: character to be shifted
-        :param int shift_amount: the amount of shift in vigenere cipher
-        :return: str shifted_char: char after shifting
-        """
-        original_position = self._chars.index(char)
-        shift_position = (original_position - shift_amount) % len(self._chars)
-        shifted_char = self._chars[shift_position]
-        return shifted_char
+        pass
 
     def encrypt(self, plaintext, key):
-        """
-        Encrypt the plaintext with given key
-        :param plaintext: the flag to encrypt
-        :param key: the key of the encryption
-        :return: null, store the ciphertext in self._cipher
-        """
-        # convert the flag to base64
         flag_base64 = self.string_to_base64(plaintext)
 
-        # obtain shift amounts from the key
         shift_list = self.get_shift_list(key)
 
-        # vigenere encryption
         encrypted_flag = ""
         for i in range(len(flag_base64)):
             round_shift_amount = shift_list[i % len(shift_list)]
             encrypted_flag += self.shift_char_encrypt(flag_base64[i], round_shift_amount)
 
-        # store the cipher
         self._cipher = encrypted_flag
 
     def decrypt(self, ciphertext, key):
-        """
-        Decrypt the ciphertext with given key
-        :param ciphertext: the ciphertext to decrypt
-        :param key: the key of the decryption
-        :return: null, store the plaintext in self._plain
-        """
+        pass
 
-        # obtain shift amounts from the key
-        shift_list = self.get_shift_list(key)
 
-        # vigenere decryption
-        decrypted_base64_text = ""
-        for i in range(len(ciphertext)):
-            round_shift_amount = shift_list[i % len(key)]
-            decrypted_base64_text += self.shift_char_decrypt(ciphertext[i], round_shift_amount)
+def Split_image(filein, header_file, body_file):
+    image_in = open(filein, "r")
+    header_out = open(header_file, "w")
+    body_out = open(body_file, "w")
 
-        # convert from base64 to utf-8 string
-        decrypted_flag = self.base64_to_string(decrypted_base64_text)
+    header = ""
+    body = ""
 
-        # store the plaintext
-        self._plain = decrypted_flag
+    next_line = image_in.readline()
+    for i in range(4):
+        header += next_line
+        next_line = image_in.readline()
+
+    while next_line:
+        body += next_line
+        next_line = image_in.readline()
+
+    header_out.write(header)
+    body_out.write(body)
+
+    image_in.close()
+    header_out.close()
+    body_out.close()
+
+
+def To_binary(string):
+    l, m = [], []
+    for i in string:
+        l.append(ord(i))
+    for j in l:
+        m.append(bin(j)[2:])
+    for k in range(len(m)):
+        if len(m[k]) != 8:
+            temp = 8 - len(m[k])
+            m[k] = '0' * temp + m[k]
+    return m
+
+
+def Embed_one(string, text):
+    b = text.split()
+    for i in range(8):
+
+        temp = bin(int(b[i]))
+
+        temp = temp[:-1] + string[i]
+
+        b[i] = str(int(temp, 2))
+
+    new_b = ' '.join(b)
+    return new_b
+
+
+def Embed(cipher, filein, fileout):
+    fin = open(filein, 'r')
+    fout = open(fileout, 'w')
+
+    Binary_list = To_binary(cipher)
+
+    round = len(Binary_list)
+    start = 0
+    text = fin.readline()
+    while start < round:
+        string = Binary_list[start]
+
+        to_write = Embed_one(string, text)
+        fout.write(to_write + '\n')
+
+        start = start + 1
+        text = fin.readline()
+
+    while text != '':
+        fout.write(text)
+
+        text = fin.readline()
+
+    fin.close()
+    fout.close()
+
+def Combine_image(header_file, body_file, combined_file):
+    header_in = open(header_file, "r")
+    body_in = open(body_file, "r")
+    fileout = open(combined_file, "w")
+
+    next_header_line = header_in.readline()
+    while next_header_line:
+        fileout.write(next_header_line)
+        next_header_line = header_in.readline()
+
+    next_body_line = body_in.readline()
+    while next_body_line:
+        fileout.write(next_body_line)
+        next_body_line = body_in.readline()
+
+    header_in.close()
+    body_in.close()
+    fileout.close()
 
 if __name__ == "__main__":
+    flag = "fcs22{wahhhhhhhh_you_found_the_flag!!!whooohooo}"
+    key = "counterstrike"
     a = VariantVigenere()
     a.encrypt(flag, key)
     ciphertext = a.get_cipher()
-    print(ciphertext)
-    a.decrypt(ciphertext, key)
-    plaintext = a.get_plain()
-    print(plaintext)
+
+    Split_image("mona_lisa.ascii_origin.pgm", "header_orig.txt", "body_orig.txt")
+    Embed(ciphertext, "body_orig.txt", "body_modified.txt")
+    Combine_image("header_orig.txt", "body_modified.txt", "mona_lisa_modified.pgm")
+
+
+
+
 
